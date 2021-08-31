52adcda62bed8073812e495bd04e5b248a60b013
uelescou <85132198+uelescou@users.noreply.github.com>
2021-08-30 23:15:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let a = 25
+
+let b = 30
+
+console.log (a + b)
+
+
+console.log (a - b)
+
+
+console.log (a * b)
+
+
+console.log (a / b)
+
+ 
+console.log (a % b)
+
+
+console.log a ** b)
+ 
+
+console.log (a === b)
+
+
+console.log (a > b)
